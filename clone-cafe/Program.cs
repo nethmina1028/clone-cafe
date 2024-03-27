c91bcf27e9071b0c2b071fa12044c3e846f3bb81
Wickramasinghe W A N P <139307869+nethmina1028@users.noreply.github.com>
2024-03-27 11:55:35 +0530

    ane mnda

--- clone-cafe/Program.cs
+++ clone-cafe/Program.cs
@@ -16,7 +16,7 @@ namespace clone_cafe
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new login());
+            Application.Run(new Form1());
         }
     }
 }
